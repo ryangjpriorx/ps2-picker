91ad1a42c446072fc746e6c531db2a2bf8fd4d5b
ryangjpriorx <ryangjpriorx@gmail.com>
2026-05-01 11:25:06 -0400

    v0.1.17: L2 BIOS hint + cache manager visual overhaul

--- ps2-checker.py
+++ ps2-checker.py
@@ -9,7 +9,7 @@
      python3 ps2-checker.py --terminal    Force terminal mode (no GUI)
 """
 
-VERSION = '0.1.16'
+VERSION = '0.1.17'
 
 import os, sys, platform, shutil, subprocess, importlib.util, json, string, time, math
 try:
