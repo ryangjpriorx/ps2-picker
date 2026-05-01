b27ad5d22faf626a80124354c7df22fbc04c450f
ryangjpriorx <ryangjpriorx@gmail.com>
2026-05-01 14:15:41 -0400

    v0.1.29: fix: Games goes straight to game list, 3-choice dialog if no card mounted, safe no-card play

--- ps2-checker.py
+++ ps2-checker.py
@@ -10,7 +10,7 @@
      python3 ps2-checker.py --skip-update Skip the update check
 """
 
-VERSION = '0.1.28'
+VERSION = '0.1.29'
 
 import os, sys, platform, shutil, subprocess, importlib.util, json, string, time, math, re
 try:
