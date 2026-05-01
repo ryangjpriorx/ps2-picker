a12c55176b255555bffd5f6958370af485fb4066
ryangjpriorx <ryangjpriorx@gmail.com>
2026-05-01 12:08:52 -0400

    v0.1.22: feat: Update Channel toggle in Settings with restart confirmation

--- ps2-checker.py
+++ ps2-checker.py
@@ -10,7 +10,7 @@
      python3 ps2-checker.py --skip-update Skip the update check
 """
 
-VERSION = '0.1.21'
+VERSION = '0.1.22'
 
 import os, sys, platform, shutil, subprocess, importlib.util, json, string, time, math, re
 try:
